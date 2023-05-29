4995826696bddf0fc31534c851ac275301af80b4
MasonNugget <133155900+MasonNugget@users.noreply.github.com>
2023-05-28 22:35:39 -0500

    Add files via upload

--- strings.js
+++ strings.js
@@ -1,31 +1,52 @@
-function strings()
-{
-    var string1 = document.getElementById("iniStr").value
-    var string2 = document.getElementById("sndStr").value
-    alert(string1);
-    alert(string2);
+function validateName(firstName, lastName) {
+  const nameRegex = /^[a-zA-Z]+$/; // Regular expression for letters only
 
-    var str3 = string1 + string2;//this variable combines strings 1 and 2 to create string 3
-    
+  if (firstName.trim() === '' || lastName.trim() === '') {
+    return false; // Empty first or last name
+  }
 
-   var rev = str3;
-   var splitString = str3.split("");//this variable splits string 3 apart
-   var reverseArray = splitString.reverse();//after splitting, this variable reverses string 3
-   alert(rev);
-   var joinArray = reverseArray.join("");//after reversing, this variable then joins string 3 back together
-   alert(joinArray);
+  if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+    return false; // Invalid characters in first or last name
+  }
 
+  if (firstName.length + lastName.length <= 20) {
+    return false; // Name length less than or equal to 20 characters
+  }
+  const fullName = firstName + lastName;
+  if (fullName.length > 20) {
+    alert('Name length exceeds 20 characters. Please enter a shorter name.');
+    return false; // Name length exceeds 20 characters
+  }
 
-   if (joinArray == str3)//after the actions above are performed, if the word is spelt the same forwards and backwards, you will get the following 2 prompts below
-   {
-    alert("This is a Palindrome")
-    alert("Please enter another word")
-   }
-   if (joinArray !== str3)//after the actions above are performed, if the word is NOT spelt the same forwards and backwards, you will get the following 2 prompts below
-   {
-    alert("This is not a plaindrome")
-    alert("Please enter another word")
-   }
+  return true; // Valid input
+}
+
+function validateAndSubmit() {
+  const firstNameInput = document.getElementById('firstName').value;
+  const lastNameInput = document.getElementById('lastName').value;
+
+  const isValidName = validateName(firstNameInput, lastNameInput);
+
+  if (!isValidName) {
+    const errorContainer = document.getElementById('errorContainer');
+    errorContainer.textContent = 'Invalid first or last name. Please enter letters only and ensure both fields are not empty.';
+    return; // Stop form submission if validation fails
+  }
+
+  alert('Valid name. Proceeding with form submission.');
+  // Proceed with form submission or further processing
+  console.log('Form submitted successfully!');
+}
+function validateZipCode(zipCode) {
+  const zipCodeRegex = /^\d{5}$/; // Regular expression for 5 digits
+
+  if (!zipCodeRegex.test(zipCode)) {
+    return false; // Invalid zip code format
+  }
+
+  return true; // Valid input
 }
 
 
+
+  
